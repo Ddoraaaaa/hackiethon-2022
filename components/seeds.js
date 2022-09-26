9896ad60738c216aea06e1d1a625127a3b3fc6e4
Ddoraaaaa <ilbp7024@gmail.com>
2022-09-27 00:32:07 +1000

    thing

---
+++ components/seeds.js
@@ -0,0 +1,47 @@
+export const PROJECTS = [
+    {
+        img: "placekitten.com/200/200",
+        name: "Cool project 1",
+        brief: "Project to use in other cool stuffs very cool cool COOL!!!",
+        time: "May 2020 - Dec 2020",
+        year: [1, 2, 3],
+    },
+    {
+        img: "placekitten.com/200/201",
+        name: "Cool project 2",
+        brief: "I like the bee movie pls love",
+        time: "May 2020 - Dec 20222",
+        year: [2, 3],
+    },
+    {
+        img: "placekitten.com/201/201",
+        name: "Cool project 3",
+        brief: "Peepeepoopoo bgfeuyuibadefbh dwiuef iufe iue fhiuefw u hefw huifew",
+        time: "May 2020 - Dec 20222",
+        year: [1, 2],
+    },
+];
+
+export const EXP = [
+    {
+        img: "placekitten.com/128/128",
+        name: "Optiver",
+        brief: "I developed some things for them and they used those shit yeah haha!",
+        time: " May 2021 - May 2022",
+        year: [1, 2],
+    },
+    {
+        img: "placekitten.com/127/128",
+        name: "Cat place",
+        brief: "I love cats. I probably don't eat them. Probably.",
+        time: "Jan 2020 - Feb 2021",
+        year: [1],
+    },
+    {
+        img: "placekitten.com/128/127",
+        name: "IntegraDev",
+        brief: "They pay a lot. Well the CEO is a sweet person",
+        time: "Jan 2019 - Feb 2999",
+        year: [2, 3],
+    }
+]
